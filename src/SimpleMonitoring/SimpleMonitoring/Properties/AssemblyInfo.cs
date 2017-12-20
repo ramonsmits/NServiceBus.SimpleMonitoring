5ca1001dc959852773426adf145d75a67a64a906
Ramon Smits <ramon.smits@gmail.com>
2017-12-20 15:15:28 +0100

    vs2017 project file for nuget packages.

--- src/SimpleMonitoring/SimpleMonitoring/Properties/AssemblyInfo.cs
+++ src/SimpleMonitoring/SimpleMonitoring/Properties/AssemblyInfo.cs
@@ -1,10 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("NServiceBus.SimpleMonitoring")]
-[assembly: AssemblyDescription("Metrics that can act as early warning for your ops/devops teams.")]
-[assembly: AssemblyConfiguration("https://github.com/ramonsmits/NServiceBus.SimpleMonitoring/")]
-[assembly: AssemblyCompany("https://github.com/ramonsmits")]
-[assembly: AssemblyProduct("NServiceBus [6,7)")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
