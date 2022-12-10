44a304dd1f34d85a95ef199afb86cfdb47eb2bed
Ramon Smits <ramon.smits@gmail.com>
2022-12-10 02:21:48 +0100

    Optimize `Log.DebugFormat` invocations by caching `IsDebugEnabled`

--- src/SimpleMonitoring/SimpleMonitoring/TrackProcessingDurationBehavior.cs
+++ src/SimpleMonitoring/SimpleMonitoring/TrackProcessingDurationBehavior.cs
@@ -8,6 +8,7 @@ using NServiceBus.Transport;
 class TrackProcessingDurationBehavior : Behavior<ITransportReceiveContext>
 {
     readonly ILog Log = LogManager.GetLogger(SimpleMonitoringFeature.LoggerName);
+    readonly bool IsDebugEnabled;
     readonly ConcurrentDictionary<IncomingMessage, DateTime> Messages;
     readonly TimeSpan Threshold;
 
@@ -15,6 +16,8 @@ class TrackProcessingDurationBehavior : Behavior<ITransportReceiveContext>
     {
         Messages = messages;
         Threshold = threshold;
+
+        IsDebugEnabled = Log.IsDebugEnabled;
     }
 
     public override async Task Invoke(ITransportReceiveContext context, Func<Task> next)
@@ -31,7 +34,7 @@ class TrackProcessingDurationBehavior : Behavior<ITransportReceiveContext>
         {
             Messages.TryRemove(instance, out start);
             var duration = DateTime.UtcNow - start;
-            Log.DebugFormat("Message '{0}' total processing duration: '{1:g}'", instance.MessageId, duration);
+            if (IsDebugEnabled) Log.DebugFormat("Message '{0}' total processing duration: '{1:g}'", instance.MessageId, duration);
             if (duration > Threshold) Log.WarnFormat("Message '{0}' total processing duration ({1:g}) is larger than the threshold '{2:g}'.", instance.MessageId, duration, Threshold);
         }
     }
