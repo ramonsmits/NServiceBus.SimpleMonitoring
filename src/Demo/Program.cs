61912f3571177701544c66e3356441ce2b59fe08
Ramon Smits <ramon.smits@gmail.com>
2024-06-14 14:28:27 +0200

    ✨ Support NServiceBus 9.x

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,49 +1,40 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus;
 
-class Program
+NServiceBus.Logging.LogManager.Use<NServiceBus.Logging.DefaultFactory>().Level(NServiceBus.Logging.LogLevel.Debug);
+var cfg = new EndpointConfiguration("Demo.SimpleMonitoring");
+cfg.UseSerialization(new SystemJsonSerializer());
+cfg.EnableInstallers();
+cfg.ReportLongRunningMessages(TimeSpan.FromSeconds(1));
+var t = cfg.UseTransport<LearningTransport>();
+cfg.PurgeOnStartup(true);
+Console.WriteLine(" F - Fast message, processing duration 0.1s");
+Console.WriteLine(" S - Slow message, processing duration 3s");
+Console.WriteLine(" ESC - Exit");
+
+var instance = await Endpoint.Start(cfg);
+
+try
 {
-    static async Task Main()
+    while (true)
     {
-#if NETCOREAPP2_0
-        Console.Title = "core";
-#endif
-        NServiceBus.Logging.LogManager.Use<NServiceBus.Logging.DefaultFactory>().Level(NServiceBus.Logging.LogLevel.Debug);
-        var cfg = new EndpointConfiguration("Demo.SimpleMonitoring");
-        cfg.EnableInstallers();
-        cfg.ReportLongRunningMessages(TimeSpan.FromSeconds(1));
-        var t = cfg.UseTransport<LearningTransport>();
-        cfg.PurgeOnStartup(true);
-        Console.WriteLine(" F - Fast message, processing duration 0.1s");
-        Console.WriteLine(" S - Slow message, processing duration 3s");
-        Console.WriteLine(" ESC - Exit");
-
-        var instance = await Endpoint.Start(cfg).ConfigureAwait(false);
-
-        try
+        switch (Console.ReadKey().Key)
         {
-            while (true)
-            {
-                switch (Console.ReadKey().Key)
-                {
-                    case ConsoleKey.Escape:
-                        return;
-                    case ConsoleKey.F:
-                        await instance.SendLocal(new TestMessage { Duration = TimeSpan.FromSeconds(0.1) }).ConfigureAwait(false);
-                        break;
-                    default:
-                        await instance.SendLocal(new TestMessage { Duration = TimeSpan.FromSeconds(3) }).ConfigureAwait(false);
-                        break;
-                }
-            }
-        }
-        finally
-        {
-            await instance.Stop().ConfigureAwait(false);
+            case ConsoleKey.Escape:
+                return;
+            case ConsoleKey.F:
+                await instance.SendLocal(new TestMessage { Duration = TimeSpan.FromSeconds(0.1) });
+                break;
+            default:
+                await instance.SendLocal(new TestMessage { Duration = TimeSpan.FromSeconds(3) });
+                break;
         }
     }
+}
+finally
+{
+    await instance.Stop();
 }
 
 class TestMessage : IMessage
@@ -56,8 +47,7 @@ class TestHandler : IHandleMessages<TestMessage>
     public async Task Handle(TestMessage message, IMessageHandlerContext context)
     {
         Console.WriteLine($"Processing for {message.Duration}");
-        await Task.Delay(message.Duration, context.CancellationToken).ConfigureAwait(false);
+        await Task.Delay(message.Duration, context.CancellationToken);
         Console.WriteLine("Done!");
     }
 }
-
